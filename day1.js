e7c3fa31afb02b8f74440c2488dfe63cff84f73a
BerraFFS <99559718+BerraFFS@users.noreply.github.com>
2022-03-07 00:31:51 +0100

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+  let g = color('green');
+  let r = color('red');
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < 41; i++){
+    for (let j = 0;j<41;j++){
+      if (Math.floor(Math.random()*2)===1){
+        fill(color('blue'));
+        rect(20*i,20*j,20,20)
+      }
+      else{
+        fill(color('red'));
+        rect(20*i,20*j,20,20)
+      }
+      
+    }
+  }
+}
